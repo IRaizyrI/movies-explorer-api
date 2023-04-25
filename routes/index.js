b1c355b4380d031e430ce4d0552c504b6299d9a1
Raizyr <raizyr@raizyr.ru>
2023-04-25 19:31:07 +0300

    another possible fix for 401

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ const { loginUser, createUser } = require('../controllers/users');
 const NotFoundError = require('../errors/not-found-err');
 const { signupValidator, loginValidator } = require('../middlewares/validation');
 
-router.post('/signup', signupValidator, createUser);
-router.post('/signin', loginValidator, loginUser);
-router.use('/signout', auth, logoutRoutes);
-router.use('/', auth, moviesRoutes);
-router.use('/', auth, usersRoutes);
-router.use('', auth, (req, res, next) => {
+router.post('/api/signup', signupValidator, createUser);
+router.post('/api/signin', loginValidator, loginUser);
+router.use('/api/signout', auth, logoutRoutes);
+router.use('/api', auth, moviesRoutes);
+router.use('/api', auth, usersRoutes);
+router.use('/api', auth, (req, res, next) => {
   next(new NotFoundError('Not found'));
 });
 module.exports = router;
